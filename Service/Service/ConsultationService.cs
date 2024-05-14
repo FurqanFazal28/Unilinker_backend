406dd4c2b1a040037d43202b7554e2f3d3110099
FurqanFazal28 <furqan.fazal444@gmail.com>
2024-05-14 17:53:06 +0500

    Secret hiding

--- Service/Service/ConsultationService.cs
+++ Service/Service/ConsultationService.cs
@@ -9,24 +9,26 @@ using OpenAI_API;
 using Core.Data.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Core.Data.DTO;
+using Microsoft.Extensions.Configuration;
 namespace Service.Service
 {
     public class ConsultationService
     {
         private readonly ApplicationDbContext context;
-
+        private readonly IConfiguration _configuration;
         public ConsultationService()
         {
         }
 
-        public ConsultationService(ApplicationDbContext context)
+        public ConsultationService(ApplicationDbContext context, IConfiguration config)
         {
             this.context = context;
+            _configuration = config;
         }
         public async Task<string> GetConsultation(string response)
         {
             string outputResult = "";
-            var openai = new OpenAIAPI("");
+            var openai = new OpenAIAPI(_configuration.GetSection("OpenAPI:Key").Value);
             CompletionRequest completionRequest = new CompletionRequest();
             completionRequest.Prompt = response;
             completionRequest.Model = OpenAI_API.Models.Model.ChatGPTTurboInstruct;
