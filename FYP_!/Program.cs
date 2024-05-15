1f39d08c4502bbf710b3e1f0b7701936c113b8ef
Huzieelothbrok <88132018+Huzieelothbrok@users.noreply.github.com>
2024-05-16 02:04:58 +0500

    finalcorschange

--- FYP_!/Program.cs
+++ FYP_!/Program.cs
@@ -45,7 +45,7 @@ if (app.Environment.IsDevelopment())
 // Use CORS middleware with specific settings
 app.UseCors(builder =>
 {
-    builder.WithOrigins("http://localhost:3000", "http://localhost:7100", "http://localhost:5173", "https://discussionforum-mhqd49pbb-huzaifas-projects-c4dd683b.vercel.app")
+    builder.WithOrigins("http://localhost:3000", "http://localhost:7100", "http://localhost:5173", "https://discussionforum-mhqd49pbb-huzaifas-projects-c4dd683b.vercel.app", "https://discussionforum-liard.vercel.app")
     .AllowCredentials()
     
            .AllowAnyHeader()
